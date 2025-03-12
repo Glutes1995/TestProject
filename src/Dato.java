0e423c32b5b529db3803df1584615f15a1cb5e34
Glutes1995 <jynke58@hotmail.com>
2025-03-12 10:11:37 +0100

    Added Dato

---
+++ src/Dato.java
@@ -0,0 +1,14 @@
+public class Dato {
+    public static void main(String[] args) {
+
+        //mine variable med datatype, navn og værdi
+        int year = 2025;
+        int date = 29;
+        String month = "Jan";
+        String day = "Wednesday";
+
+        //mine outputs der vises i terminalen
+        System.out.println("Amerikansk format: " + day + ", " + month + " " + date + ", " + year + ".");
+        System.out.println("Euro format: " + day + ", " + date + " " + month + ", " + year + ".");
+    }
+}
