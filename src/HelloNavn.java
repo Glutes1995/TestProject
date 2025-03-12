2cd54863746cdfb10c5c8ebb941634094e88879f
Glutes1995 <jynke58@hotmail.com>
2025-03-12 10:10:11 +0100

    Added HelloWorld

---
+++ src/HelloNavn.java
@@ -0,0 +1,10 @@
+public class HelloNavn {
+    public static void main(String[] args) {
+
+        //min variabel med datatype, navn og værdi
+        String martin = "Martin";
+
+        //mit output der vises i terminalen
+        System.out.println("Hej " + martin + "!");
+    }
+}
